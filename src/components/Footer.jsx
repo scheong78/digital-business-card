ff5178a00b6fd55181925dfeb0910f28749ec429
Stanley Cheong <scheong78@gmail.com>
2024-01-27 22:10:09 -0500

    hid the icons to resolve deployment errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,17 @@
-import twitter from "../assets/Twitter Icon.png"
-import facebook from "../assets/Facebook Icon.png"
-import instagram from "../assets/Instagram Icon.png"
-import github from "../assets/Github Icon.png"
-import linkedin from "../assets/Linkedin Icon.png"
+// import twitter from "../assets/Twitter Icon.png"
+// import facebook from "../assets/Facebook Icon.png"
+// import instagram from "../assets/Instagram Icon.png"
+// import github from "../assets/Github Icon.png"
+// import linkedin from "../assets/Linkedin Icon.png"
 
 export default function Footer () {
     return (
         <div className="footer">
-            <a href="#"><img src={twitter} alt="twitter icon" /></a>
+            {/* <a href="#"><img src={twitter} alt="twitter icon" /></a>
             <a href="#"><img src={facebook} alt="facebook icon" /></a>
             <a href="#"><img src={instagram} alt="instagram icon" /></a>
             <a href="#"><img src={linkedin} alt="linkedin icon" /></a>
-            <a href="#"><img src={github} alt="github icon" /></a>
+            <a href="#"><img src={github} alt="github icon" /></a> */}
         </div>
     )
 }
