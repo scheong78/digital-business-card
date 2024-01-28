ef80304897af2c15226e1b5e785fd1288c44759d
Stanley Cheong <scheong78@gmail.com>
2024-01-28 09:33:16 -0500

    added back facebook icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import twitter from "../assets/Twitter Icon.png"
-// import facebook from "../assets/Facebook Icon.png"
+import facebook from "../assets/Facebook Icon.png"
 // import instagram from "../assets/Instagram Icon.png"
 // import github from "../assets/Github Icon.png"
 // import linkedin from "../assets/Linkedin Icon.png"
@@ -8,10 +8,10 @@ export default function Footer () {
     return (
         <div className="footer">
             <a href="#"><img src={twitter} alt="twitter icon" /></a>
-            {/* <a href="#"><img src={facebook} alt="facebook icon" /></a>
-            <a href="#"><img src={instagram} alt="instagram icon" /></a>
+            <a href="#"><img src={facebook} alt="facebook icon" /></a>
+            {/* <a href="#"><img src={instagram} alt="instagram icon" /></a>
             <a href="#"><img src={linkedin} alt="linkedin icon" /></a>
-            <a href="#"><img src={github} alt="github icon" /></a> */}
+            <a href="#"><img src={github} alt="github icon" /></a>  */}
         </div>
     )
 }
