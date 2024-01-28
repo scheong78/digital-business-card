f015aba1c96b7d473a115c9136207321d2389667
Stanley Cheong <scheong78@gmail.com>
2024-01-28 09:31:07 -0500

    testing out icons for errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-// import twitter from "../assets/Twitter Icon.png"
+import twitter from "../assets/Twitter Icon.png"
 // import facebook from "../assets/Facebook Icon.png"
 // import instagram from "../assets/Instagram Icon.png"
 // import github from "../assets/Github Icon.png"
@@ -7,8 +7,8 @@
 export default function Footer () {
     return (
         <div className="footer">
-            {/* <a href="#"><img src={twitter} alt="twitter icon" /></a>
-            <a href="#"><img src={facebook} alt="facebook icon" /></a>
+            <a href="#"><img src={twitter} alt="twitter icon" /></a>
+            {/* <a href="#"><img src={facebook} alt="facebook icon" /></a>
             <a href="#"><img src={instagram} alt="instagram icon" /></a>
             <a href="#"><img src={linkedin} alt="linkedin icon" /></a>
             <a href="#"><img src={github} alt="github icon" /></a> */}
